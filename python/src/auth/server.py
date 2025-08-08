0f8e42a21d41ea8da12b856b03a83492aafa6b82
Kevin Ndolo <kevindev1@outlook.com>
2025-08-08 08:43:43 +0300

    Set up MySQL connection parameters using Flask config and dotenv for secure environment management

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,18 +1,24 @@
+from dotenv import load_dotenv
 import jwt, datetime, os
 from flask import Flask, request
 from flask_mysqldb import MySQL
 
-# Initialize Flask app and MySQL connection
-# Ensure you have the necessary environment variables set for MySQL connection
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize Flask app 
 server = Flask(__name__)
-mysql = MySQL(server)
 
 # configure MySQL connection parameters
-server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
-server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
-server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
-server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
+server.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+server.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT"))
+
+# Initialize MySQL. this must done after setting the config
+mysql = MySQL(server)
 
 
 @server.route("/login", methods=["POST"])
@@ -21,56 +27,6 @@ def login():
     if not auth:
         return "missing credentials", 401
 
-    # check db for username and password
-    cur = mysql.connection.cursor()
-    res = cur.execute(
-        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
-    )
-
-    if res > 0:
-        user_row = cur.fetchone()
-        email = user_row[0]
-        password = user_row[1]
-
-        if auth.username != email or auth.password != password:
-            return "invalid credentials", 401
-        else:
-            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
-    else:
-        return "invalid credentials", 401
-
-
-@server.route("/validate", methods=["POST"])
-def validate():
-    encoded_jwt = request.headers["Authorization"]
-
-    if not encoded_jwt:
-        return "missing credentials", 401
-
-    encoded_jwt = encoded_jwt.split(" ")[1]
-
-    try:
-        decoded = jwt.decode(
-            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
-        )
-    except:
-        return "not authorized", 403
-
-    return decoded, 200
-
-
-def createJWT(username, secret, authz):
-    return jwt.encode(
-        {
-            "username": username,
-            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
-            + datetime.timedelta(days=1),
-            "iat": datetime.datetime.utcnow(),
-            "admin": authz,
-        },
-        secret,
-        algorithm="HS256",
-    )
 
 
 if __name__ == "__main__":
